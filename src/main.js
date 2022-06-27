01d0f866fb4a0fbba00e075085e9d2e08126be5a
borgerry <borodingerry@gmail.com>
2022-06-27 11:38:02 +0300

    Merge 'main' into 'borodin'

---
+++ src/main.js
@@ -0,0 +1,9 @@
+/**
+ * imports
+ */
+import './css/main.scss';
+// import Cookies from 'js-cookie';
+
+document.addEventListener('DOMContentLoaded', function () {
+  window.console.log('Hello from main.js 👋.');
+});
