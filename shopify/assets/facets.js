19a6a56da38fbd12444f79e512642a9d6ccdcea8
Yaroslav-nogin <ynagin@splitdev.pro>
2022-08-16 17:18:41 +0300

    latest 16.08.2022

--- shopify/assets/facets.js
+++ shopify/assets/facets.js
@@ -75,6 +75,7 @@ class FacetFiltersForm extends HTMLElement {
 
   static renderProductGridContainer(html) {
     document.getElementById('ProductGridContainer').innerHTML = new DOMParser().parseFromString(html, 'text/html').getElementById('ProductGridContainer').innerHTML;
+    setColumns();
   }
 
   static renderProductCount(html) {
